20717ee273689bfe21aee80ec9ee695fdac9a6a7
s19911 <s19911@pjwstk.edu.pl>
2025-03-16 20:49:45 +0100

    Dodanie testu Calc

---
+++ ConsoleApp1xd/AverageCalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+public class AverageCalculatorTests
+{
+    [Fact]
+    public void CalculateAverage_ReturnsCorrectAverage()
+    {
+        int[] numbers = { 2, 4, 6, 8, 10 };
+        double result = AverageCalculator.CalculateAverage(numbers);
+        Assert.Equal(6.0, result);
+    }
+
+    [Fact]
+    public void CalculateAverage_ThrowsExceptionForEmptyArray()
+    {
+        Assert.Throws<ArgumentException>(() => AverageCalculator.CalculateAverage(new int[] { }));
+    }
+}
