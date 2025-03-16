ff39a92cc9ac9953a525e975166dcba2c1307a79
s19911 <s19911@pjwstk.edu.pl>
2025-03-16 20:55:41 +0100

    Dodano maxcalc test

---
+++ ConsoleApp1xd/MaxCalculatorTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+public class MaxCalculatorTests
+{
+    [Fact]
+    public void FindMax_ReturnsCorrectMaxValue()
+    {
+        int[] numbers = { 2, 4, 6, 8, 10 };
+        int result = MaxCalculator.FindMax(numbers);
+        Assert.Equal(10, result);
+    }
+
+    [Fact]
+    public void FindMax_ThrowsExceptionForEmptyArray()
+    {
+        Assert.Throws<ArgumentException>(() => MaxCalculator.FindMax(new int[] { }));
+    }
+}
