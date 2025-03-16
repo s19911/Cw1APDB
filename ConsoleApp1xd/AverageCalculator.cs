dd9c0877e721c81aba370336366bbf75ffb2901b
s19911 <s19911@pjwstk.edu.pl>
2025-03-16 20:48:24 +0100

    Initial Galaz dodanie AverageCalculator

---
+++ ConsoleApp1xd/AverageCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class AverageCalculator
+{
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Tablica nie może być pusta");
+        }
+
+        int sum = 0;
+        foreach (var num in numbers)
+        {
+            sum += num;
+        }
+
+        return (double)sum / numbers.Length;
+    }
+}
