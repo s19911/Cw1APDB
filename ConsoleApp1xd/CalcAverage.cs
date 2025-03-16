a08a3a7f172bd1ba09bc7846def310fe83d591d1
s19911 <s19911@pjwstk.edu.pl>
2025-03-16 21:15:29 +0100

    konflikt

---
+++ ConsoleApp1xd/CalcAverage.cs
@@ -0,0 +1,15 @@
+﻿public static double CalculateAverage(int[] numbers)
+{
+    if (numbers == null || numbers.Length == 0)
+    {
+        throw new ArgumentException("Tablica nie może być pusta");
+    }
+
+    int sum = 0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+
+    return (double)sum / numbers.Length;
+}
