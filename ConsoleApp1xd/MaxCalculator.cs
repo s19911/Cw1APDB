ced87702e619823e4e8fd8762655b65bad9f265a
s19911 <s19911@pjwstk.edu.pl>
2025-03-16 20:54:01 +0100

    Dodano maxcalc

---
+++ ConsoleApp1xd/MaxCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class MaxCalculator
+{
+    public static int FindMax(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Tablica nie może być pusta");
+        }
+
+        int max = numbers[0];
+        foreach (var num in numbers)
+        {
+            if (num > max)
+                max = num;
+        }
+
+        return max;
+    }
+}
